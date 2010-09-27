1693047857f09f55171bd7d9576bed237e794ff6
eafigueira <eafigueira@c23259cc-097c-65d2-0a43-0db8315d37ea>
2010-09-27 12:45:50 +0000

    Metodos novos

--- trunk/trafegusws/src/java/br/com/chapecosolucoes/trafegusweb/server/ws/TrafegusWS.java
+++ trunk/trafegusws/src/java/br/com/chapecosolucoes/trafegusweb/server/ws/TrafegusWS.java
@@ -4,8 +4,11 @@
  */
 package br.com.chapecosolucoes.trafegusweb.server.ws;
 
+import br.com.chapecosolucoes.trafegusweb.server.beans.CrefClasseReferencia;
 import br.com.chapecosolucoes.trafegusweb.server.beans.UsuaUsuario;
 import br.com.chapecosolucoes.trafegusweb.server.jpa.JPAUtil;
+import java.util.ArrayList;
+import java.util.List;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -33,6 +36,43 @@ public class TrafegusWS {
             result = (UsuaUsuario) q.getSingleResult();
         } catch (Exception e) {
             result = new UsuaUsuario();
+        }
+        return result;
+    }
+
+    /**
+     * Operação de serviço web
+     */
+    @WebMethod(operationName = "solicitaClassesReferencias")
+    public ArrayList<CrefClasseReferencia> solicitaClassesReferencias(@WebParam(name = "codEmrpesa") Integer codEmrpesa) {
+        EntityManager em = JPAUtil.getInstance().getEntityManager();
+        Query q = em.createQuery("SELECT c FROM CrefClasseReferencia c WHERE c.cref_pess_oras_codigo = :cref_pess_oras_codigo");
+        q.setParameter("cref_pess_oras_codigo", codEmrpesa);
+
+        ArrayList<CrefClasseReferencia> result = null;
+        try {
+            result = (ArrayList<CrefClasseReferencia>) q.getResultList();
+        } catch (Exception e) {
+            result = new ArrayList<CrefClasseReferencia>();
+        }
+        return result;
+    }
+
+    /**
+     * Operação de serviço web
+     */
+    @WebMethod(operationName = "solicitaReferencias")
+    public ArrayList<CrefClasseReferencia> solicitaReferencias(@WebParam(name = "codEmpresa") Integer codEmpresa, @WebParam(name = "codClasseReferencia") Integer codClasseReferencia) {
+        EntityManager em = JPAUtil.getInstance().getEntityManager();
+        Query q = em.createQuery("SELECT c FROM CrefClasseReferencia c WHERE (c.cref_pess_oras_codigo = null or c.cref_pess_oras_codigo = :cref_pess_oras_codigo) and (c.crefCodigo = :crefCodigo)");
+        q.setParameter("cref_pess_oras_codigo", codEmpresa);
+        q.setParameter("crefCodigo", codClasseReferencia);
+
+        ArrayList<CrefClasseReferencia> result = null;
+        try {
+            result = (ArrayList<CrefClasseReferencia>) q.getResultList();
+        } catch (Exception e) {
+            result = new ArrayList<CrefClasseReferencia>();
         }
         return result;
     }
