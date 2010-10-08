2296524d431f45a17f59c13a2d6cfe5895c9f116
estevan.diedrich <estevan.diedrich@c23259cc-097c-65d2-0a43-0db8315d37ea>
2010-10-08 01:46:06 +0000

    Tentativa de encapsular o restorno de uma nativeQuery em um VO usando a annotation SqlResultSetMapping

---
+++ trafegusws/src/java/br/com/chapecosolucoes/trafegusweb/server/vo/VeiculoVO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.chapecosolucoes.trafegusweb.server.vo;
+
+import java.io.Serializable;
+import javax.persistence.ColumnResult;
+import javax.persistence.EntityResult;
+import javax.persistence.FieldResult;
+import javax.persistence.SqlResultSetMapping;
+
+/**
+ *
+ * @author estevan
+ */
+@SqlResultSetMapping(name="VeiculoVO",
+entities=@EntityResult(
+          entityClass=VeiculoVO.class,
+          fields=@FieldResult(name="placaVeiculo", column="circuito")),
+    columns=@ColumnResult(name="resCount")
+)
+public class VeiculoVO implements Serializable{
+    private String placaVeiculo;
+    private String tipoVeiculoDescricao;
+
+    /**
+     * @return the placaVeiculo
+     */
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
+
+    /**
+     * @param placaVeiculo the placaVeiculo to set
+     */
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    /**
+     * @return the tipoVeiculoDescricao
+     */
+    public String getTipoVeiculoDescricao() {
+        return tipoVeiculoDescricao;
+    }
+
+    /**
+     * @param tipoVeiculoDescricao the tipoVeiculoDescricao to set
+     */
+    public void setTipoVeiculoDescricao(String tipoVeiculoDescricao) {
+        this.tipoVeiculoDescricao = tipoVeiculoDescricao;
+    }
+}
